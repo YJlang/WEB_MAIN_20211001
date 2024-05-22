02fdc8a7054bb5da426fef9240fc2032053e1636
YJlang <wnsgk1114@naver.com>
2024-05-22 13:10:18 +0900

    0522

--- js/login.js
+++ js/login.js
@@ -32,6 +32,7 @@ function init() {
         emailInput.value = savedId;
         idsave_check.checked = true;
     }
+    session_check(); //세션 유무 검사
 }
 
 // 로그인 시도 및 로그인 시도 횟수 증가
@@ -113,6 +114,9 @@ function checkInput() {
         alert(`쿠키값 : ${emailValue}`);
     }
 
+    // 세션 생성
+    session_set();
+
     // 폼 제출
     loginForm.submit();
 }
@@ -149,6 +153,39 @@ function getFailedLoginCount() {
 function setFailedLoginCount(count) {
     setCookie("failedLoginCount", count, 1); // 로그인 실패 횟수 설정
 }
+
+// 세션 저장
+function session_set(){
+    let session_id = document.querySelector("#typeEmailX");
+    if(sessionStorage){
+        sessionStorage.setItem("Session_Storage_test", session_id.value);
+    }
+    else{
+        alert("로컬 스토리지 지원X");
+    }
+}
+
+// 세션 읽기
+function session_get(){
+    if(sessionStorage){
+        return sessionStorage.getItem("Session_Storage_test");
+    }
+    else{
+        alert("세션 스토리지 지원 X");
+    }
+}
+
+//세션 검사
+function session_check(){
+    if(sessionStorage.getItem("Session_Storage_test")){
+        alert("이미 로그인 되었습니다.");
+        location.href = `../login/index_login.html`; //로그인된 페이지로 이동
+    }
+
+}
+
+// 페이지 로딩 시 초기화 함수 호출
+document.addEventListener("DOMContentLoaded", init);
 
 // 로그인 버튼 클릭 시 로그인 처리
 document.getElementById("login_btn").addEventListener('click', login);
